f3d875003efae4cb6b6a9b829088da702fbf092c
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2022-01-06 22:03:54 +0900

    create molecules-news-and-event-more

---
+++ src/stories/molecules-news-and-events-more.stories.js
@@ -0,0 +1,6 @@
+import MoleculesNewsAndEventsMore from "../components/molecules/news-and-events-more/news-and-events-more.html";
+import "../components/molecules/news-and-events-more/news-and-events-more.scss";
+
+export default { title: "Molecules/news-and-events/more" };
+
+export const NewsAndEventsMore = () => MoleculesNewsAndEventsMore;
