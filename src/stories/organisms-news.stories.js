3976d72a196aae8ec5527e7c15bff3eaea3251b4
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2022-01-11 23:15:34 +0900

    init

---
+++ src/stories/organisms-news.stories.js
@@ -0,0 +1,6 @@
+import OrganismsNews from "../components/organisms/news/news.html";
+import "../components/organisms/news/news";
+
+export default { title: "Organisms/News" };
+
+export const News = () => OrganismsNews;
