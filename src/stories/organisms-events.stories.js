59bc66d9d28a84535705c574f7f1a279cfc95a06
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2022-01-12 10:45:18 +0900

    stash

---
+++ src/stories/organisms-events.stories.js
@@ -0,0 +1,6 @@
+import OrganismsEvents from "../components/organisms/events/events.html";
+import "../components/organisms/events/events.scss";
+
+export default { title: "Organisms/Events" };
+
+export const Events = () => OrganismsEvents;
