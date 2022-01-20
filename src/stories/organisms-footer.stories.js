82417f400d159e29fe90ff6b83b886a625f9db4a
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2022-01-20 22:48:56 +0900

    mdの完成

---
+++ src/stories/organisms-footer.stories.js
@@ -0,0 +1,6 @@
+import OrganismsFooter from "../components/organisms/footer/footer.html";
+import "../components/organisms/footer/footer.scss";
+
+export default { title: "Organisms/Footer" };
+
+export const Footer = () => OrganismsFooter;
